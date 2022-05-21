4822ad7376a439c19bbffce722f11c7e00d80136
Sergey_Stepanov <cmi52@mail.ru>
2022-05-21 09:26:54 +0300

    ДЗ 1.9 выполнил

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        Author author1 = new Author("Lev", "TolstoyW");
+        Author author2 = new Author("Alexandr", "Pushkin");
+
+
+        Book book1 = new Book("Война и мир", author1, 1900);
+        Book book2 = new Book("У лукоморья", author2, 1901);
+
+        book2.setYear(1902);
+
 
     }
 }
